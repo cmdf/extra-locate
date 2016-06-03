30d37f70eb9582c26ae9a5bf7b823a7848350cca
Subhajit Sahu <wolfram77@gmail.com>
2016-06-04 04:57:35 +0530

    program: int32 can be renamed as int (simpler)

--- cs-locate/Program.cs
+++ cs-locate/Program.cs
@@ -5,8 +5,8 @@ namespace orez.locate {
 		static void Main(string[] args) {
 			var x = args.Length >= 1 ? args[0] : "+0";
 			var y = args.Length >= 2 ? args[1] : "+0";
-			var xv = (x[0] == '+' || x[0] == '-' ? Console.CursorLeft : 0) + Int32.Parse(x);
-			var yv = (y[0] == '+' || y[0] == '-' ? Console.CursorTop : 0) + Int32.Parse(y);
+			var xv = (x[0] == '+' || x[0] == '-' ? Console.CursorLeft : 0) + int.Parse(x);
+			var yv = (y[0] == '+' || y[0] == '-' ? Console.CursorTop : 0) + int.Parse(y);
 			if(args.Length == 0) Console.WriteLine(xv + " " + yv);
 			else Console.SetCursorPosition(xv < 0 ? 0 : xv, yv < 0 ? 0 : yv);
 		}
