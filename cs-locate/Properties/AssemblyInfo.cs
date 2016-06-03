d45695140f43868ed9da7ae78c8e0008d8f50f96
Subhajit Sahu <wolfram77@gmail.com>
2016-06-04 05:11:05 +0530

    project: assembly info updated (description)

--- cs-locate/Properties/AssemblyInfo.cs
+++ cs-locate/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("olocate")]
-[assembly: AssemblyDescription("Get or set Cursor position in Windows Console")]
+[assembly: AssemblyDescription("Get or set Cursor location in Windows Console")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Orez")]
 [assembly: AssemblyProduct("olocate")]
